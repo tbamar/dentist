b2a387485cb0ad2a56b970d63228966751a3bf8f
theamarverma <amarverma.lnct@gmail.com>
2025-04-17 14:04:08 +0530

    Add components for clinic overview, doctor details, and treatment cards; enhance booking and contact pages

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,20 +2,30 @@ import HeaderSection from './_components/HeaderSection';
 import AboutSection from './_components/AboutSection';
 import ServiceCard from './_components/ServiceCard';
 import WorkProcess from './_components/WorkProcess';
+import DoctorDetails from './_components/DoctorDetails';
+import DoctorCards from './_components/DoctorDetails';
+import TreatmentCards from '../services/_components/Demo';
+import ClinicOverview from './_components/ClinicOverview';
 export default function HomePage() {
 	return (
 		<main className="pt-24">
-			<section>
+			<section className="pb-24">
 				<HeaderSection />
 			</section>
-			<section className="px-10">
-				<AboutSection />
+			<section className="px-10 ">
+				<ClinicOverview />
 			</section>
-			<section className="grid grid-cols-3 gap-10">
-				<ServiceCard />
+			<section className="px-10 py-24">
+				<h1 className="text-5xl font-bold flex justify-center items-center ">
+					Meet Our Doctors
+				</h1>
+				<DoctorCards />
 			</section>
-			<section className="px-10">
-				<WorkProcess />
+			<section className="px-10 pb-24 ">
+				<h1 className="text-5xl font-bold flex justify-center items-center ">
+					Treatments We Provide:
+				</h1>
+				<TreatmentCards />
 			</section>
 		</main>
 	);

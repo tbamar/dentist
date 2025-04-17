6fb32d3250cda5d36041024bb29844e7e2a17bb4
theamarverma <amarverma.lnct@gmail.com>
2025-04-17 14:04:15 +0530

    Add Banner component to the booking appointment page

--- src/app/book-appointment/page.tsx
+++ src/app/book-appointment/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import AppointmentForm from './_components/AppointmentForm';
+import Banner from '../_components/global/Banner';
 
 const page = () => {
 	return (
 		<div className="pt-24">
+			<Banner name="Book Appointment" />
 			<AppointmentForm />
 		</div>
 	);

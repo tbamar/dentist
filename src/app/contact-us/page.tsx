9591dfd392ce05ed666150992101cc7cd77867c6
theamarverma <amarverma.lnct@gmail.com>
2025-04-17 14:04:48 +0530

    Add Contact Us page with Banner component

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Banner from '../_components/global/Banner';
 
 const page = () => {
-	return <div>page</div>;
+	return (
+		<div className="pt-24">
+			<Banner name="Contact Us" />
+			<section></section>
+		</div>
+	);
 };
 
 export default page;

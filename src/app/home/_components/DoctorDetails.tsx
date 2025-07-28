e090c6acb0c9b6f417ef862c2bc4e82e0b98f0f1
theamarverma <amarverma.lnct@gmail.com>
2025-07-28 20:47:19 +0530

    torsha_saibal_img

--- src/app/home/_components/DoctorDetails.tsx
+++ src/app/home/_components/DoctorDetails.tsx
@@ -13,7 +13,7 @@ interface Doctor {
 const doctors: Doctor[] = [
 	{
 		name: 'Dr. Torsha Ray Bhattacharyya',
-		imageUrl: '/images/profile/saibal.jpg',
+		imageUrl: '/images/profile/torsha.png',
 		qualifications:
 			'BDS, KLE Institute of Dental Science, Bangalore; MDS (Gold Medalist) in Oral Medicine & Radiodiagnosis, AECS Maaruti Institute of Dental Sciences',
 		training: [
@@ -30,7 +30,7 @@ const doctors: Doctor[] = [
 
 	{
 		name: 'Dr. Saibal Bhattacharyya',
-		imageUrl: '/images/profile/torsha.png',
+		imageUrl: '/images/profile/saibal.jpg',
 		qualifications:
 			'BDS, MS Ramaiah Dental College, Bangalore; MDS in Oral & Maxillofacial Surgery, Awadh Dental College & Hospital, Jamshedpur',
 		training: [
